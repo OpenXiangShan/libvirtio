c0ba495e71ab91f8308bb1ce513b4ea57ba0127e
Yaxing Guo <guoyaxing@bosc.ac.cn>
2026-06-20 20:33:51 +0800

    backend: fix VNC keyboard keycodes
    Signed-off-by: Yaxing Guo <guoyaxing@bosc.ac.cn>

--- backend/virtio_backend_ui_vnc.c
+++ backend/virtio_backend_ui_vnc.c
@@ -69,16 +69,105 @@ static int parse_vnc_listen(const char *listen, char **host, int *port)
 
 static int keycode_from_keysym(rfbKeySym key)
 {
-	if (key >= 'a' && key <= 'z')
-		return KEY_A + (int)(key - 'a');
-	if (key >= 'A' && key <= 'Z')
-		return KEY_A + (int)(key - 'A');
-	if (key >= '1' && key <= '9')
-		return KEY_1 + (int)(key - '1');
-	if (key == '0')
-		return KEY_0;
-
 	switch (key) {
+	case XK_a:
+	case XK_A:
+		return KEY_A;
+	case XK_b:
+	case XK_B:
+		return KEY_B;
+	case XK_c:
+	case XK_C:
+		return KEY_C;
+	case XK_d:
+	case XK_D:
+		return KEY_D;
+	case XK_e:
+	case XK_E:
+		return KEY_E;
+	case XK_f:
+	case XK_F:
+		return KEY_F;
+	case XK_g:
+	case XK_G:
+		return KEY_G;
+	case XK_h:
+	case XK_H:
+		return KEY_H;
+	case XK_i:
+	case XK_I:
+		return KEY_I;
+	case XK_j:
+	case XK_J:
+		return KEY_J;
+	case XK_k:
+	case XK_K:
+		return KEY_K;
+	case XK_l:
+	case XK_L:
+		return KEY_L;
+	case XK_m:
+	case XK_M:
+		return KEY_M;
+	case XK_n:
+	case XK_N:
+		return KEY_N;
+	case XK_o:
+	case XK_O:
+		return KEY_O;
+	case XK_p:
+	case XK_P:
+		return KEY_P;
+	case XK_q:
+	case XK_Q:
+		return KEY_Q;
+	case XK_r:
+	case XK_R:
+		return KEY_R;
+	case XK_s:
+	case XK_S:
+		return KEY_S;
+	case XK_t:
+	case XK_T:
+		return KEY_T;
+	case XK_u:
+	case XK_U:
+		return KEY_U;
+	case XK_v:
+	case XK_V:
+		return KEY_V;
+	case XK_w:
+	case XK_W:
+		return KEY_W;
+	case XK_x:
+	case XK_X:
+		return KEY_X;
+	case XK_y:
+	case XK_Y:
+		return KEY_Y;
+	case XK_z:
+	case XK_Z:
+		return KEY_Z;
+	case XK_1:
+		return KEY_1;
+	case XK_2:
+		return KEY_2;
+	case XK_3:
+		return KEY_3;
+	case XK_4:
+		return KEY_4;
+	case XK_5:
+		return KEY_5;
+	case XK_6:
+		return KEY_6;
+	case XK_7:
+		return KEY_7;
+	case XK_8:
+		return KEY_8;
+	case XK_9:
+		return KEY_9;
+	case XK_0:
+		return KEY_0;
 	case XK_Escape:
 		return KEY_ESC;
 	case XK_Return:
